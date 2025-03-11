5f4fd5e7f4ed2f096c5cbc996b0a9fae52b8e89c
Aleem246 <21ag1a6640@gmail.com>
2025-03-11 20:19:47 +0530

    Number of Substrings Containing All Three Characters

--- 1358_Number_of_Substrings_Containing_All_Three_Characters.java
+++ 1358_Number_of_Substrings_Containing_All_Three_Characters.java
@@ -1,31 +1,31 @@
 class Solution {
     public int numberOfSubstrings(String s) {
-        int strt=0,n= s.length();
-        int a=0,b=0,c=0,count=0;
-        for(int end=0; end<n; end++){
+        int strt = 0, n = s.length();
+        int a = 0, b = 0, c = 0, count = 0;
+        for (int end = 0; end < n; end++) {
             char C = s.charAt(end);
-            if(C=='a')
+            if (C == 'a')
                 a++;
-            else if(C=='b')
+            else if (C == 'b')
                 b++;
             else
                 c++;
-            while(a>=1 && b>=1 && c>=1){
+            while (a >= 1 && b >= 1 && c >= 1) {
                 // System.out.println(n-end);
-                count+= n-end;
+                count += n - end;
                 char strtc = s.charAt(strt);
-                if(strtc=='a')
+                if (strtc == 'a')
                     a--;
-                else if(strtc=='b')
+                else if (strtc == 'b')
                     b--;
                 else
                     c--;
-                    
-                strt++; 
+
+                strt++;
             }
 
         }
         return count;
-        
+
     }
 }
